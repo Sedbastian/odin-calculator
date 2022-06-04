4e6ee5dda9afcac63179ce15e2699eab180b406c
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-06-04 12:37:11 -0300

    Agregar operaciones e igual

--- javascript.js
+++ javascript.js
@@ -1,10 +1,38 @@
+let auxiliarOperando = "";
+let operacionElegida = "";
 let primerOperando = "";
+let resultado;
 
 function popularDisplay(event) {
-    primerOperando = `${primerOperando}` + `${event.target.textContent}`;
-    display.textContent = primerOperando;
+    auxiliarOperando = `${auxiliarOperando}` + `${event.target.textContent}`;
+    display.textContent = auxiliarOperando;
+}
+
+function elegirOperacion(event) {
+    operacionElegida = `${event.target.textContent}`;
+    primerOperando = auxiliarOperando;
+    auxiliarOperando = "";
+}
+
+function ejecutarIgual() {
+    if (operacionElegida === "+") {
+        resultado = parseFloat(primerOperando) + parseFloat(auxiliarOperando);
+    } else if (operacionElegida ==="-") {
+        resultado = parseFloat(primerOperando) - parseFloat(auxiliarOperando);
+    } else if (operacionElegida ==="*") {
+        resultado = parseFloat(primerOperando) * parseFloat(auxiliarOperando);
+    } else if (operacionElegida ==="/") {
+        resultado = parseFloat(primerOperando) / parseFloat(auxiliarOperando);
+    }
+    display.textContent = `${resultado}`;
 }
 
 const display = document.querySelector("#display");
 const digitos = document.querySelectorAll(".digitos div");
 digitos.forEach((digito) => digito.addEventListener("click", popularDisplay));
+
+const operaciones = document.querySelectorAll(".operacion");
+operaciones.forEach((operacion) => operacion.addEventListener("click", elegirOperacion));
+
+const igual = document.querySelector("#igual");
+igual.addEventListener("click", ejecutarIgual);
