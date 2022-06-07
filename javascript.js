0ec96832787971bde6ec94fe6a7ee20a7dd0b0ce
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-06-07 17:23:52 -0300

    Agregar event.preventDefault() a teclaApretada()

--- javascript.js
+++ javascript.js
@@ -111,7 +111,7 @@ function ejecutarBackspace () {
 };
 
 function teclaApretada (event) {
-    console.log(event.key);
+    event.preventDefault();
     // Si la tecla es un digito o un punto (y no hay ya un punto): 
     if (parseInt(event.key) + 1 || (event.key === "." && !auxiliarOperando.includes("."))) {
         popularDisplay(event.key);
